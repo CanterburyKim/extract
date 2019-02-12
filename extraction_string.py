2596b51f3376e6d40a83f6af045cfe1af027a77b
Goro Kim <31192175+CanterburyKim@users.noreply.github.com>
2019-02-12 08:41:05 -0500

    Create extraction_string.py

---
+++ extraction_string.py
@@ -0,0 +1,31 @@
+
+# TODO: write your function defs here
+
+
+
+
+
+
+
+
+# TEST CASES BEGIN HERE
+
+class TestVowels(unittest.TestCase):
+    """
+    test class
+    """
+
+    first_string = 'helixablegosnowmobile'
+    second_string = 'mrXwhatAwonderfulGuyIluftballoons'
+
+
+    def test_vowels_A(self):
+
+        extract_A(first_string, second_string)
+
+    # TODO: add other test cases needed to test the functions
+
+
+
+if __name__ == '__main__':
+    unittest.main()
